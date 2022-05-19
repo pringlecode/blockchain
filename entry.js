481a61e1b2bf7a5fff45960055c8e6e69edd48b1
John Pringle <pringlecode@gmail.com>
2022-05-19 06:43:05 +0100

    Adds transactions

--- entry.js
+++ entry.js
@@ -1,4 +1,13 @@
 var SHA256 = require('crypto-js/sha256');
+var Transaction = /** @class */ (function () {
+    function Transaction(sender, receiver, amount, message) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.message = message;
+    }
+    return Transaction;
+}());
 var Block = /** @class */ (function () {
     function Block(index, previousHash, data) {
         this.index = index;
@@ -16,32 +25,64 @@ var Block = /** @class */ (function () {
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        console.log("\nBlock Mined!\n\tSolution:\t".concat(this.nonce, "\n\tBlock Number:\t").concat(this.index, "\n\tMessage:\t").concat(this.data['message']));
+        this.display();
+    };
+    Block.prototype.display = function () {
+        console.log("\nBlock Mined!\n\tSolution:\t".concat(this.nonce, "\n\tBlock Number:\t").concat(this.index));
+        console.log(this.data);
     };
     return Block;
 }());
 var BlockChain = /** @class */ (function () {
     function BlockChain() {
         this.chain = [];
+        this.pendingTransactions = [];
         this.chain = [];
-        this.difficulty = 5;
+        this.difficulty = 4;
         this.createGenesisBlock();
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
     BlockChain.prototype.createGenesisBlock = function () {
-        this.addBlock({ message: "Genesis Block" });
+        this.minePendingTransactions(null);
     };
     BlockChain.prototype.getLatestBlock = function () {
         return this.chain[this.chain.length - 1];
     };
     BlockChain.prototype.getLatestBlockHash = function () {
-        return this.chain.length !== 0 ? this.getLatestBlock().hash : '';
+        return this.chain.length !== 0 ? this.getLatestBlock().hash : 'genesis';
     };
-    BlockChain.prototype.addBlock = function (data) {
+    BlockChain.prototype.minePendingTransactions = function (miningRewardAddress) {
+        /**
+         * TODO Allow miners to choose the best transactions
+         */
         var index = this.chain.length;
         var previousHash = this.getLatestBlockHash();
-        var block = new Block(index, previousHash, data);
+        var block = new Block(index, previousHash, {
+            transactions: this.pendingTransactions
+        });
         block.mineBlock(this.difficulty);
         this.chain.push(block);
+        this.pendingTransactions = [new Transaction("blockchain", miningRewardAddress, this.miningReward, "mining reward")];
+    };
+    BlockChain.prototype.createTransaction = function (transaction) {
+        this.pendingTransactions.push(transaction);
+    };
+    BlockChain.prototype.getBalanceOfAddress = function (address) {
+        var balance = 0;
+        for (var _i = 0, _a = this.chain; _i < _a.length; _i++) {
+            var block = _a[_i];
+            for (var _b = 0, _c = block.data.transactions; _b < _c.length; _b++) {
+                var transaction = _c[_b];
+                if (transaction.sender === address) {
+                    balance -= transaction.amount;
+                }
+                if (transaction.receiver === address) {
+                    balance += transaction.amount;
+                }
+            }
+        }
+        return balance;
     };
     BlockChain.prototype.isChainValid = function () {
         for (var i = 1; i < this.chain.length; i++) {
@@ -62,10 +103,18 @@ var BlockChain = /** @class */ (function () {
 // Create the block chain
 var MyBlockChain = new BlockChain();
 // Simulate block creation
-MyBlockChain.addBlock({ message: 'Second Block' });
-MyBlockChain.addBlock({ message: 'Third Block' });
-//console.log(JSON.stringify(MyBlockChain, null, 1));
+// MyBlockChain.addBlock({ message: 'Second Block' });
+// MyBlockChain.addBlock({ message: 'Third Block' });
+// console.log(JSON.stringify(MyBlockChain, null, 1));
+MyBlockChain.createTransaction(new Transaction('John Smith', 'Pizza Palace', 10000, 'one pineapple pizza, please!'));
+MyBlockChain.createTransaction(new Transaction('Simon Williams', 'John Smith', 100, 'I ate 1% of your pizza so have 100 back'));
+MyBlockChain.minePendingTransactions('Harry Potter');
+MyBlockChain.createTransaction(new Transaction('John Smith', 'Phone repair shop', 50, 'Thanks for fixing my phone!'));
+MyBlockChain.createTransaction(new Transaction('Simon Williams', 'John Smith', 300, 'I ate 3% of your pizza so have 300 back'));
+MyBlockChain.minePendingTransactions('Donald Duck');
+console.log("balance of John: ".concat(MyBlockChain.getBalanceOfAddress('John Smith')));
 console.log("\nIs Blockchain valid?\t ".concat(MyBlockChain.isChainValid()));
+// console.log(JSON.stringify(MyBlockChain, null, 1));
 // Change the blockchain to ensure validation fails
 // MyBlockChain.chain[1].data.message = "Hello World"
 // console.log(JSON.stringify(MyBlockChain, null, 1));
--- entry.ts
+++ entry.ts
@@ -1,5 +1,23 @@
 const SHA256 = require('crypto-js/sha256');
 
+
+
+class Transaction { 
+
+    sender: string;
+    receiver: string;
+    amount: number;
+    message: string;
+
+    constructor(sender: string, receiver: string, amount: number, message: string) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.message = message;
+    }
+
+}
+
 class Block { 
     
     index: number;
@@ -18,33 +36,42 @@ class Block {
         this.hash = this.calculateHash();
     }
     
-    calculateHash() { 
+    calculateHash() : string { 
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
     }
 
-    mineBlock(difficulty: number) { 
+    mineBlock(difficulty: number) : void { 
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) { 
             this.nonce++;
             this.hash = this.calculateHash()
         }
-        console.log(`\nBlock Mined!\n\tSolution:\t${this.nonce}\n\tBlock Number:\t${this.index}\n\tMessage:\t${this.data['message']}`)
+        this.display();
     }
 
+    display() : void {
+        console.log(`\nBlock Mined!\n\tSolution:\t${this.nonce}\n\tBlock Number:\t${this.index}`)
+        console.log(this.data);
+    }
+    
 }
 
 class BlockChain { 
     
     chain = []; 
     difficulty: number;
+    pendingTransactions = [];
+    miningReward: number;
 
     constructor() {
         this.chain = [];
-        this.difficulty = 5;
+        this.difficulty = 4;
         this.createGenesisBlock();
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
-    createGenesisBlock() : void { 
-        this.addBlock({ message: "Genesis Block" });
+    createGenesisBlock(): void { 
+        this.minePendingTransactions(null);
     }
 
     getLatestBlock() : Block { 
@@ -52,18 +79,43 @@ class BlockChain {
     }
 
     getLatestBlockHash() : string { 
-        return this.chain.length !== 0 ? this.getLatestBlock().hash : '';
+        return this.chain.length !== 0 ? this.getLatestBlock().hash : 'genesis';
     }
 
-    addBlock(data: object) : void {
+    minePendingTransactions(miningRewardAddress: string): void {
+       /**
+        * TODO Allow miners to choose the best transactions
+        */
         let index : number = this.chain.length;
         let previousHash: string = this.getLatestBlockHash();
-        let block: Block = new Block(index, previousHash, data);
+        let block: Block = new Block(index, previousHash, {
+            transactions: this.pendingTransactions
+        });
         block.mineBlock(this.difficulty);
         this.chain.push(block);
+        this.pendingTransactions = [new Transaction(`blockchain`, miningRewardAddress, this.miningReward, `mining reward`)]
     }
 
-    isChainValid() { 
+    createTransaction(transaction: Transaction): void { 
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address: string) : number { 
+        let balance: number = 0;
+        for (const block of this.chain) {
+            for (const transaction of block.data.transactions) {
+                if (transaction.sender === address) { 
+                    balance -= transaction.amount;
+                }
+                if (transaction.receiver === address) {
+                    balance += transaction.amount;
+                }
+            }
+        }
+        return balance;
+    }
+
+    isChainValid() : boolean { 
         for (let i = 1; i < this.chain.length; i++) { 
             const currentBlock : Block = this.chain[i];
             const previousBlock : Block = this.chain[i - 1];
@@ -80,15 +132,39 @@ class BlockChain {
 };
 
 // Create the block chain
-const MyBlockChain = new BlockChain();
+let MyBlockChain: BlockChain = new BlockChain();
 
 // Simulate block creation
-MyBlockChain.addBlock({ message: 'Second Block' });
-MyBlockChain.addBlock({ message: 'Third Block' });
-//console.log(JSON.stringify(MyBlockChain, null, 1));
+// MyBlockChain.addBlock({ message: 'Second Block' });
+// MyBlockChain.addBlock({ message: 'Third Block' });
+// console.log(JSON.stringify(MyBlockChain, null, 1));
+
+MyBlockChain.createTransaction(
+    new Transaction('John Smith', 'Pizza Palace', 10000, 'one pineapple pizza, please!')
+)
+MyBlockChain.createTransaction(
+    new Transaction('Simon Williams', 'John Smith', 100, 'I ate 1% of your pizza so have 100 back')
+)
+
+MyBlockChain.minePendingTransactions('Harry Potter')
+
+MyBlockChain.createTransaction(
+    new Transaction('John Smith', 'Phone repair shop', 50, 'Thanks for fixing my phone!')
+)
+MyBlockChain.createTransaction(
+    new Transaction('Simon Williams', 'John Smith', 300, 'I ate 3% of your pizza so have 300 back')
+)
+
+MyBlockChain.minePendingTransactions('Donald Duck')
+
+console.log(`balance of John: ${MyBlockChain.getBalanceOfAddress('John Smith')}`);
 
 
 console.log(`\nIs Blockchain valid?\t ${MyBlockChain.isChainValid()}`);
+
+// console.log(JSON.stringify(MyBlockChain, null, 1));
+
+
 
 // Change the blockchain to ensure validation fails
 // MyBlockChain.chain[1].data.message = "Hello World"
